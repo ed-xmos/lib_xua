ff2fb2da0acc178069b12fca73096a4f660a421e
Ed <ed@xmos.com>
2023-12-12 11:36:48 +0000

    Wrapper uses _2 versions

--- examples/dual_explorer/src/xua_xud_wrapper.c
+++ examples/dual_explorer/src/xua_xud_wrapper.c
@@ -18,7 +18,7 @@ int XUD_Main_wrapper(chanend c_epOut[], int noEpOut,
 }
 
 // XUA does not currently allow C to call XUA_Endpoint0
-extern void XUA_Endpoint0(  chanend c_ep0_out,
+extern void XUA_Endpoint0_2(  chanend c_ep0_out,
                             chanend c_ep0_in,
                             NULLABLE_RESOURCE(chanend, c_audioCtrl),
                             NULLABLE_RESOURCE(chanend, c_mix_ctl),
@@ -34,11 +34,11 @@ void XUA_Endpoint0_wrapper( chanend c_ep0_out,
                             NULLABLE_RESOURCE(chanend, c_EANativeTransport_ctrl),
                             NULLABLE_RESOURCE(chanend, dfuInterface))
 {
-    XUA_Endpoint0(c_ep0_out, c_ep0_in, c_audioCtrl, c_mix_ctl, c_clk_ctl, c_EANativeTransport_ctrl, dfuInterface);
+    XUA_Endpoint0_2(c_ep0_out, c_ep0_in, c_audioCtrl, c_mix_ctl, c_clk_ctl, c_EANativeTransport_ctrl, dfuInterface);
 }
 
 // XUA does not currently allow C to call XUA_Buffer
-void XUA_Buffer(chanend c_ep_out_1,
+void XUA_Buffer_2(chanend c_ep_out_1,
                 chanend c_ep_in_2,
                 chanend c_ep_in_1,
                 chanend c_sof,
@@ -54,11 +54,11 @@ void XUA_Buffer_wrapper(chanend c_ep_out_1,
                         IN_PORT p_for_mclk_count,
                         chanend c_aud)
 {
-    XUA_Buffer(c_ep_out_1, c_ep_in_2, c_ep_in_1, c_sof, c_aud_ctl, p_for_mclk_count, c_aud);
+    XUA_Buffer_2(c_ep_out_1, c_ep_in_2, c_ep_in_1, c_sof, c_aud_ctl, p_for_mclk_count, c_aud);
 }
 
 // XUA does not currently allow C to call XUA_AudioHub
-void XUA_AudioHub(chanend c_aud,
+void XUA_AudioHub_2(chanend c_aud,
                 xcore_clock_t clk_audio_mclk,
                 xcore_clock_t clk_audio_bclk,
                 IN_PORT p_mclk_in,
@@ -76,5 +76,5 @@ void XUA_AudioHub_wrapper(  chanend c_aud,
                             out_buffered_port_32_t p_i2s_dac[],
                             in_buffered_port_32_t p_i2s_adc[])
 {
-    XUA_AudioHub(c_aud, clk_audio_mclk, clk_audio_bclk, p_mclk_in, p_lrclk, p_bclk, p_i2s_dac, p_i2s_adc);
+    XUA_AudioHub_2(c_aud, clk_audio_mclk, clk_audio_bclk, p_mclk_in, p_lrclk, p_bclk, p_i2s_dac, p_i2s_adc);
 }
